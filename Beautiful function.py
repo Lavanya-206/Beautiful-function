29aa3273abb8f95ef8bc1ba4d2381fe91e0163c9
Lavanya-206 <84123257+Lavanya-206@users.noreply.github.com>
2021-05-22 21:32:13 +0530

    Create Beautiful function.py

---
+++ Beautiful function.py
@@ -0,0 +1,13 @@
+#Beautiful function code#
+#code goes like this#
+
+n=input()
+total=0
+i=len(n)-1
+carry=0
+While(i>0):
+    curr_dist=10-(int(num[i]+carry)
+    total=total+curr_dist
+    carry=1
+total=total+9
+Print(total)
